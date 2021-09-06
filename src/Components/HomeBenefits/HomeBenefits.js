d84a4e5e5fe1d635b7464654eaa8162886ff6a37
Javier Martinez Ribo <javiermribo@gmail.com>
2021-09-06 10:55:59 -0300

    Se añadió img de las distintas tarjetas. Testeando comportamiento en responsive

--- src/Components/HomeBenefits/HomeBenefits.js
+++ src/Components/HomeBenefits/HomeBenefits.js
@@ -1,3 +1,12 @@
+import './HomeBenefits.css';
+import visaLogo from './Images/visaLogo.png';
+import mastercardLogo from './Images/mastercardLogo.png';
+import americanExpressLogo from './Images/americanExpressLogo.png';
+import ahora12 from './Images/ahora12.svg';
+import ahora18 from './Images/ahora18.svg';
+import ahora24 from './Images/ahora24.svg';
+import ahora30 from './Images/ahora30.svg';
+
 const HomeBenefits = () => {
   return (
     <section className="homeBenefitsContainer px-2">
@@ -5,12 +14,17 @@ const HomeBenefits = () => {
         Paga menos y comprá más, con estos increíbles descuentos
       </h1>
       <section className="ahoraBenefits my-4">
-        <h3>Ahora 30, 24, 18 y 12</h3>
+        <h3>Ahora 12, 18, 24 y 30</h3>
         <p>
           Solo para productos seleccionados con Visa, Master, Cabal y Amex
           bancarias.
-          
         </p>
+        <div className="ahoraPlanBenefits mb-2">
+          <img src={ahora12} className="ahoraPlaceholder mx-2" />
+          <img src={ahora18} className="ahoraPlaceholder mx-2" />
+          <img src={ahora24} className="ahoraPlaceholder mx-2" />
+          <img src={ahora30} className="ahoraPlaceholder mx-2" />
+        </div>
         <a>
           <small>Ver términos y condiciones</small>
         </a>
@@ -20,12 +34,18 @@ const HomeBenefits = () => {
         <h6>
           Solo en productos seleccionados y sujeto a disponibilidad en stock
         </h6>
+        <div className="card-images">
+          <img src={visaLogo} className="cardImgPlaceholder mx-2" />
+          <img src={mastercardLogo} className="cardImgPlaceholder mx-2" />
+          <img src={americanExpressLogo} className="cardImgPlaceholder mx-2" />
+        </div>
       </section>
       <section className="bankTransferBenefits my-4">
-        <h3>¡Hasta 15% de descuento en todos los prductos haciendo transferencia bancaria!</h3>
-        <h6>
-          Comprá eso que tanto queres, sin pagar de mas.
-        </h6>
+        <h3>
+          ¡Hasta 15% de descuento en todos los prductos haciendo transferencia
+          bancaria!
+        </h3>
+        <h6>Comprá eso que tanto queres, sin pagar de mas.</h6>
       </section>
     </section>
   );
