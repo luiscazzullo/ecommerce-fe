47151437e64926e8be17ef4c69a26345980fce52
Javier Martinez Ribo <javiermribo@gmail.com>
2021-09-10 11:39:07 -0300

    Sección de beneficios s/ headerWithoutUser y searchBar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import Footer from './Components/Footer/Footer';
 import HomeBenefits from './Components/HomeBenefits/HomeBenefits';
 import HeaderWhitOutUser from './Components/headerWithOutUser/headerWhitOutUser';
+import Card from './Components/Card/Card';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         </Switch>
       </Router>
           <HomeBenefits/>
+          <Card/>
           <Footer/>
     </>
     );
