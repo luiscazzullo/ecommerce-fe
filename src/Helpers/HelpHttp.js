59146643111c7c258d5429b7f7e648c5cc284fa3
Javier Martinez Ribo <javiermribo@gmail.com>
2021-09-13 11:58:45 -0300

    Creando servidor de prueba, para mostrar contenido en Card

---
+++ src/Helpers/HelpHttp.js
@@ -0,0 +1,13 @@
+const url = 'http://localhost:5000/items'
+
+export const HelpHttp = () => {
+  const postItem = async () => {
+    const newItem = Revisar//,
+    const response = await fetch (url )
+  
+  }
+
+  const get = () => {};
+
+  const post = () => {};
+}
